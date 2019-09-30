2d38d379b1bffd482472003868f674e2c79ca4fd
Artur Mustafin <me@experimentalcommunity.org>
2019-09-30 07:10:33 +0300

    renaming
    refactored directories

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 // Eli Grey (c) http://purl.eligrey.com/github/FileSaver.js
 // LCG Park & Miller (c) 1988,1993, s=>()=>(2**31-1&(s=Math.imul(48271,s)))/2**31
 
-import { ui } from "./ui";
+import { ui } from "./js/ui";
 
 ui();
