2bce86e3f9dfb894a537eac40865d6e8c00a6b57
Artur Mustafin <me@experimentalcommunity.org>
2019-09-30 05:41:33 +0300

    fix
    switched fo svelte rollup

--- src/index.js
+++ src/index.js
@@ -2,15 +2,6 @@
 // Eli Grey (c) http://purl.eligrey.com/github/FileSaver.js
 // LCG Park & Miller (c) 1988,1993, s=>()=>(2**31-1&(s=Math.imul(48271,s)))/2**31
 
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./App";
-
 import { ui } from "./ui";
 
 ui();
-
-ReactDOM.render(<App />, document.getElementById("app"));
-
-module.hot.accept();
